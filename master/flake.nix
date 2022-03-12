4290503b52bbd09e7f281885147524ceb24e1349
hugosenari <hugosenari@gmail.com>
2022-03-12 03:34:48 -0300

    chore: re index morepretty

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Morepretty - Does more than nimpretty.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-morepretty-master.flake = false;
+  inputs.src-morepretty-master.owner = "treeform";
+  inputs.src-morepretty-master.ref   = "refs/heads/master";
+  inputs.src-morepretty-master.repo  = "morepretty";
+  inputs.src-morepretty-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-morepretty-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-morepretty-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
