d91d500fdc3d81133ebb4c1c830917ad5aa6c688
hugosenari <hugosenari@gmail.com>
2022-03-27 18:21:00 -0300

    feat(nimpkgs): remove lock

--- 0_1_0/flake.nix
+++ 0_1_0/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-morepretty-0_1_0.ref   = "refs/tags/0.1.0";
   inputs.src-morepretty-0_1_0.owner = "treeform";
   inputs.src-morepretty-0_1_0.repo  = "morepretty";
+  inputs.src-morepretty-0_1_0.dir   = "";
   inputs.src-morepretty-0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
